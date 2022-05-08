d915c75c9f241e0d383b8a8f01bdfe842aa0ff73
alexandragulpe <gulpe.alexandra@yahoo.com>
2022-05-08 16:34:01 +0300

    Starting page

--- src/main/java/com/example/doctorappointmentapplication/HelloApplication.java
+++ src/main/java/com/example/doctorappointmentapplication/HelloApplication.java
@@ -4,20 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-
-public class HelloApplication extends Application {
+import javafx.scene.Parent;
+public class HelloApplication extends Application
+{
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+    public void start(Stage primaryStage) throws Exception
+    {
+        //Parent root = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        //StackPane root = new StackPane();
+        //root.setId("stack-pane");
+        Scene scene = new Scene(root, 1200, 800);
+        //root.setStyle("-fx-background-image:url('test.png'); -fx-background-repeat: no-repeat; -fx-background-position: center center; -fx-background-size: stretch;");
+        //primaryStage.getIcons().add(new Image("file: icon.png"));
+        //scene.getStylesheets().add(String.valueOf(this.getClass().getResource("style.css")));
 
-    public static void main(String[] args) {
-        launch();
+        primaryStage.setTitle("Doctor Appointment Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+    public static void main(String[] args)
+    {
+        launch(args);
     }
 }
--- src/main/java/com/example/doctorappointmentapplication/HelloController.java
+++ src/main/java/com/example/doctorappointmentapplication/HelloController.java
@@ -1,14 +1,50 @@
 package com.example.doctorappointmentapplication;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label labelField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private Button backButton;
+
+    public void doctorButtonOnAction(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+
+        Stage window = (Stage) loginButton.getScene().getWindow();
+        window.setScene(new Scene(root,1200,800));
+    }
+
+
+    public void patientButtonOnAction(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("register.fxml"));
+
+        Stage window = (Stage) registerButton.getScene().getWindow();
+        window.setScene(new Scene(root,1200,800));
+
+    }
+
+    public void backButton(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+
+        Stage window = (Stage) backButton.getScene().getWindow();
+        window.setScene(new Scene(root,1200,800));
+
     }
 }
