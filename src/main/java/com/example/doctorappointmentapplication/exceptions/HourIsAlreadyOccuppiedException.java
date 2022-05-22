6b907bca04222da56814b3c75d1d30a63601a2a5
mihai-glu <alexandrugluhovschi@yahoo.com>
2022-05-23 02:44:36 +0300

    Added all exceptions for verifying appointment and for accepting/deleting appointment

--- src/main/java/com/example/doctorappointmentapplication/exceptions/HourIsAlreadyOccuppiedException.java
+++ src/main/java/com/example/doctorappointmentapplication/exceptions/HourIsAlreadyOccuppiedException.java
@@ -1,4 +1,8 @@
 package com.example.doctorappointmentapplication.exceptions;
 
-public class HourIsAlreadyOccuppiedException {
+public class HourIsAlreadyOccuppiedException extends Exception {
+    public HourIsAlreadyOccuppiedException(){
+        super("Hour Occupied ! Please change hour");
+
+    }
 }
--- src/main/java/com/example/doctorappointmentapplication/exceptions/InvalidHourException.java
+++ src/main/java/com/example/doctorappointmentapplication/exceptions/InvalidHourException.java
@@ -2,7 +2,7 @@ package com.example.doctorappointmentapplication.exceptions;
 
 public class InvalidHourException extends Exception {
     public InvalidHourException() {
-        super("Please enter a valid calendar date");
+        super("Please enter a hour between 10 and 16!");
     }
 }
 
