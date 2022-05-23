556016d296244fb172e2887708b61acab1afded8
alexandragulpe <gulpe.alexandra@yahoo.com>
2022-05-23 12:06:50 +0300

    Small changes at the UI.

--- src/main/java/com/example/doctorappointmentapplication/Controllers/DoctorAppointmentsPageController.java
+++ src/main/java/com/example/doctorappointmentapplication/Controllers/DoctorAppointmentsPageController.java
@@ -4,10 +4,16 @@ import com.example.doctorappointmentapplication.exceptions.NoAppointmentSelected
 import com.example.doctorappointmentapplication.exceptions.NoDoctorCommentAddedException;
 import com.example.doctorappointmentapplication.services.AppointmentService;
 import com.example.doctorappointmentapplication.services.UserService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.util.List;
 import java.util.Objects;
@@ -28,6 +34,9 @@ public class DoctorAppointmentsPageController {
 
     @FXML
     private TextField doctorComment;
+
+    @FXML
+    private Button backButton;
 
 
     private int currentID;
@@ -101,6 +110,17 @@ public class DoctorAppointmentsPageController {
         }catch(NoDoctorCommentAddedException e){
             errorMessage.setText(e.getMessage());
         }
+
+    }
+
+    public void backButtonOnAction(ActionEvent event) throws  Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/doctorappointmentapplication/doctorPage.fxml"));
+        Parent root = loader.load();
+        DoctorPageController doctorPageController = loader.getController();
+        doctorPageController.setUsername(username);
+
+        Stage window =(Stage) backButton.getScene().getWindow();
+        window.setScene(new Scene(root,1200,800));
 
     }
 
